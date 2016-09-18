d19704fdbe7c0ae6a3683ba1a7e3116fd0728d5a
Kate Springsteen <katherine.springsteen@gmail.com>
2016-09-18 08:56:14 -0600

    updates2

--- js/app.js
+++ js/app.js
@@ -1,31 +1,45 @@
-$(document).ready(function(){
-    $('.ryu').mouseenter(function(){
-    	console.log('test');
+$(document).ready(function() {
+    $('.ryu').mouseenter(function() {
         $('.ryu-still').hide();
         $('.ryu-ready').show();
     })
+
     .mouseleave(function() {
         $('.ryu-still').show();
         $('.ryu-ready').hide();
     })
-    // .mousedown(function(){
-    //     playHadouken();
-    //     $('.ryu-ready').hide();
-    //     $('.ryu-throwing').show();
-    //     $('.hadouken').finish().show().animate(
-    //         {'left': '1020px'},
-    //         500,
-    //         function(){
-    //             $('.hadouken').hide();
-    //             $('.hadouken').css('left', '520px');
-    //         });
-    // })
-    // .mouseup(function(){
-    //     $('.ryu-throwing').hide();
-    //     $('.ryu-ready').show();
-    // });
 
+    .mousedown(function() {
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function() {
+                $('.hadouken').hide();
+                $('.hadouken').css('left', '520px');
+            });
+            })
+
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
 });
+
+$(document).keydown(function(e) {
+	if (e.keyCode == 88) {
+		$('.ryu-cool').show();
+		$('.ryu-still').hide();
+	}
+    })
+    .keyup(function(e) {
+    	if (e.keyCode == 88) {
+    		$('.ryu-cool').hide();
+    		$('.ryu-still').show();	
+    	} 
+	});
 
 function playHadouken() {
     $('#hadouken-sound')[0].volume = 0.5;
